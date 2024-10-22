06960402ccfe2ef48a06724e3aa6c915febea6a4
zayhstevens <isaiahstevens17@gmail.com>
2024-10-22 14:30:19 -0500

    Added Visual Studio Project

---
+++ Fall2024-Assignment3-iqstevens/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fall2024_Assignment3_iqstevens.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
+
---
+++ Fall2024-Assignment3-iqstevens/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Fall2024_Assignment3_iqstevens.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
