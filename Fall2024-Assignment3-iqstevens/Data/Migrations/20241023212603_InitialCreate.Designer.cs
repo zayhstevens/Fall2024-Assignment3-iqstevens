0cc8d2627cb800ad783011a5d78b610e56b996aa
zayhstevens <isaiahstevens17@gmail.com>
2024-10-23 16:30:23 -0500

    Initial migration testing

--- Fall2024-Assignment3-iqstevens/Data/Migrations/20241023212603_InitialCreate.Designer.cs
+++ Fall2024-Assignment3-iqstevens/Data/Migrations/20241023212603_InitialCreate.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Fall2024_Assignment3_iqstevens.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Fall2024_Assignment3_iqstevens.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241023212603_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.10");
--- Fall2024-Assignment3-iqstevens/obj/Debug/net8.0/Fall2024-Assignment3-iqstevens.AssemblyInfo.cs
+++ Fall2024-Assignment3-iqstevens/obj/Debug/net8.0/Fall2024-Assignment3-iqstevens.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Fall2024-Assignment3-iqstevens")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06960402ccfe2ef48a06724e3aa6c915febea6a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6d5f70e38ed5b745ac9ceb030a6724271157067")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fall2024-Assignment3-iqstevens")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fall2024-Assignment3-iqstevens")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
